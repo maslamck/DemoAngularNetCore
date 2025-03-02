32027a886d0c1a5fc3af960c7292f37b14e99d3b
Aslam <maslamck@gmail.com>
2025-03-03 02:06:13 +0400

    first commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/components/header/header.component";
+import { SidebarComponent } from "./core/components/sidebar/sidebar.component";
+import { FooterComponent } from "./core/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BlankComponent } from './blank/blank.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'blank',
+    component: BlankComponent,
+  },
+];
